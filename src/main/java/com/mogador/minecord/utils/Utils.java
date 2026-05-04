e1a5fcad3eaaf22aaaae0b03b6c9071efd460fb6
Mogadragos <alice.caillaud17@gmail.com>
2026-05-04 13:32:14 +0200

    Remove spaces

--- src/main/java/com/mogador/minecord/utils/Utils.java
+++ src/main/java/com/mogador/minecord/utils/Utils.java
@@ -1,11 +1,10 @@
 package com.mogador.minecord.utils;
 
 import java.awt.Color;
+
 import net.md_5.bungee.api.ChatColor;
 
 public class Utils {
-
-
     public static String colorize(String msg, Color color) {
         msg = String.valueOf(ChatColor.of(color)) + msg;
         return ChatColor.translateAlternateColorCodes('&', msg);
