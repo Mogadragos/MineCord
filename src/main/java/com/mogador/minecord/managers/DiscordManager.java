a6e8f73bd3ea5f4dff69e19e8b0adebeaa516299
Mogadragos <alice.caillaud17@gmail.com>
2026-05-04 13:31:20 +0200

    Use Pattern, add chunk and small fixed

--- src/main/java/com/mogador/minecord/managers/DiscordManager.java
+++ src/main/java/com/mogador/minecord/managers/DiscordManager.java
@@ -3,14 +3,17 @@ package com.mogador.minecord.managers;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
+import java.util.regex.Pattern;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.google.common.collect.Lists;
 import com.mogador.minecord.data.MessageData;
 import com.mogador.minecord.utils.DiscordWebhook;
 import com.mogador.minecord.utils.DiscordWebhook.EmbedObject;
@@ -30,7 +33,8 @@ public class DiscordManager {
     private final String ERROR_KEY_NOT_FOUND = "You need to set " + YML_WEBHOOK_URL_KEY + " in the " + SECRET_FILE_NAME + " file";
     private final String ERROR_NOT_READY = "Error in loading [MineCord], can't send messages";
     private final String ERROR_SENDING_MSG = "Error when trying to send messages";
-    private final String REGEX_DISCORD_MARKDOWN = "\\*|_|`|\\{|}|\\[|]|\\(|\\)|#|\\+|-|\\.|!|~|>";
+    private final Pattern REGEX_DISCORD_MARKDOWN = Pattern.compile("\\*|_|`|\\{|}|\\[|]|\\(|\\)|#|\\+|-|\\.|!|~|>");
+    private final int DISCORD_MAX_EMBED = 10;
 
     private boolean ready = false;
     private JavaPlugin plugin;
@@ -71,41 +75,66 @@ public class DiscordManager {
         ready = true;
     }
 
-    public void sendMessage(Player player, List<MessageData> messages) {
-        if(!ready) {
+    public void sendMessages(Player player, List<MessageData> messages) {
+        if(!ready || messages.size() == 0) {
             plugin.getLogger().warning(ERROR_NOT_READY);
             player.sendMessage(ERROR_NOT_READY);
+            return;
         }
 
-        DiscordWebhook webhook = new DiscordWebhook(url)
+        List<List<MessageData>> subSets = Lists.partition(messages, DISCORD_MAX_EMBED);
+        List<DiscordWebhook> webhooks = new ArrayList<>();
+
+        for(List<MessageData> chunk : subSets) {
+            DiscordWebhook webhook = initWebhook();
+
+            for(MessageData data : chunk) {
+                webhook.addEmbed(new EmbedObject()
+                    .setDescription(buildDescription(data.getPlayer(), data.getMessage()))
+                    .setColor(PlayerConfigManager.getInstance().getColor(data.getPlayer()))
+                    .setTimestamp(data.getTimestamp())
+                );
+            }
+
+            webhooks.add(webhook);
+        }
+
+        if(subSets.getLast().size() == DISCORD_MAX_EMBED) {
+            DiscordWebhook webhook = initWebhook();
+            webhooks.add(webhook);
+        }
+        webhooks.getLast().addEmbed(new EmbedObject().setDescription("Sent by " + buildPlayerName(player)));
+
+        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+            boolean isError = false;
+            for(DiscordWebhook webhook : webhooks) {
+                try {
+                    webhook.execute();
+                } catch (URISyntaxException | IOException e) {
+                    plugin.getLogger().log(Level.WARNING, ERROR_SENDING_MSG, e);
+                    plugin.getLogger().warning(webhook.getJsonAsString());
+                    isError = true;
+                };
+            }
+
+            if(isError) {
+                plugin.getServer().getScheduler().runTask(plugin, () -> player.sendMessage(ERROR_SENDING_MSG));
+            }
+        });
+    }
+
+    private DiscordWebhook initWebhook() {
+        return new DiscordWebhook(url)
             .setUsername(name)
             .setAvatarUrl(avatar_url)
             .setTts(isTts);
-
-        for(MessageData data : messages) {
-            webhook.addEmbed(new EmbedObject()
-                .setDescription(buildDescription(data.getPlayer(), data.getMessage()))
-                .setColor(PlayerConfigManager.getInstance().getColor(data.getPlayer()))
-                .setTimestamp(data.getTimestamp())
-            );
-        }
-
-        webhook.addEmbed(new EmbedObject().setDescription("Sent by " + buildPlayerName(player)));
-
-        try {
-            webhook.execute();
-        } catch (URISyntaxException | IOException e) {
-            plugin.getLogger().log(Level.WARNING, ERROR_SENDING_MSG, e);
-            plugin.getLogger().warning(webhook.getJsonAsString());
-            player.sendMessage(ERROR_SENDING_MSG);
-        };
     }
 
     private String buildDescription(Player player, String msg) {
 
         String description = "**";
         description += buildPlayerName(player);
-        description += msg.replaceAll(REGEX_DISCORD_MARKDOWN, "\\\\$0").replace("\\", "\\\\");
+        description += REGEX_DISCORD_MARKDOWN.matcher(msg).replaceAll("\\\\$0").replace("\\", "\\\\");
         description += "**";
 
         return description;
@@ -114,7 +143,7 @@ public class DiscordManager {
     private String buildPlayerName(Player player) {
         String discordId = PlayerConfigManager.getInstance().getDiscordId(player);
 
-        String playerName = "<" + player.getName().replaceAll(REGEX_DISCORD_MARKDOWN, "\\\\$0").replace("\\", "\\\\");
+        String playerName = "<" + REGEX_DISCORD_MARKDOWN.matcher(player.getName()).replaceAll("\\\\$0").replace("\\", "\\\\");
         if(discordId != null) {
             playerName += " <@" + discordId + ">";
         }
